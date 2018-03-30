cf0091e06f70cd057cabbaf2908b309eff02ecaf
陈奕涛 <chenyitao@215.sub-72-127-2.myvzw.com>
2018-03-30 10:40:39 +0800

    remove cache

--- worker/parser.py
+++ worker/parser.py
@@ -61,6 +61,7 @@ class Parser(object):
             self._push_new_task(task, ret.tasks)
         task.status = Task.Status.ParsedSuccess
         TaskManager().task_successed(task)
+        TaskCacheManager().delete_cachce(task)
 
     def _push_new_task(self, task, tasks):
         if not len(tasks):
